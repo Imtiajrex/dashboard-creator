91ef5bcd3a22801a4f1900a6d3737784e281cf96
imtiaj <imtiajrex@gmail.com>
2022-12-15 10:57:33 +0600

    updated navbar,hero and foooter

---
+++ src/lib/components/home/hero.svelte
@@ -0,0 +1,18 @@
+<div class="relative px-6 lg:px-8 pt-40 ">
+	<div class="mx-auto max-w-3xl pt-20 pb-32 sm:pt-48 sm:pb-40">
+		<div>
+			<div>
+				<h1 class="text-4xl font-bold text-slate-50 sm:text-center sm:text-6xl">
+					Unleashing Your Inner Artist: Practice Tools for Creativity
+				</h1>
+				<p class="mt-6 text-lg leading-8 text-slate-300 sm:text-center">
+					Explore our selection of top-quality practice tools and resources, and discover the
+					perfect tools to help you take your art to new heights
+				</p>
+				<div class="mt-8 flex gap-x-4 sm:justify-center">
+					<button class="btn btn-primary text-white w-full max-w-xs">Get Started</button>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,15 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 
+import path from 'path';
 /** @type {import('vite').UserConfig} */
 const config = {
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	resolve: {
+		alias: {
+			// these are the aliases and paths to them
+			$components: path.resolve('./src/lib/components')
+		}
+	}
 };
 
 export default config;
