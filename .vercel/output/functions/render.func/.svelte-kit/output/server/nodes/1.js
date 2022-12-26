b56157cdf9eb0230adc0288137ee075fe247e4fb
imtiaj <imtiajrex@gmail.com>
2022-12-26 21:53:00 +0600

    removed the vercel folder

--- .vercel/output/functions/render.func/.svelte-kit/output/server/nodes/1.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 1;
-export const component = async () => (await import('../entries/fallbacks/error.svelte.js')).default;
-export const file = '_app/immutable/components/error.svelte-507c223a.js';
-export const imports = ["_app/immutable/components/error.svelte-507c223a.js","_app/immutable/chunks/index-5a48f7c1.js","_app/immutable/chunks/singletons-ae45c7ac.js"];
-export const stylesheets = [];
-export const fonts = [];
--- .vercel/output/static/_app/immutable/chunks/4-304375be.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/register/_page.svelte-0f42e41a.js";export{t as component};
