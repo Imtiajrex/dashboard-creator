ad7c384a8fa654a0ff539b8b5c9a281d80881d56
Imtiajrex <imtiajrex@gmail.com>
2026-08-30 17:24:15 +0600

    Restore 2 working files dropped during malware history rewrite

---
+++ src/routes/(home)/+layout.svelte
@@ -0,0 +1,20 @@
+<script>
+	import '../../app.postcss';
+	import Footer from '$lib/utils/home/footer.svelte';
+	import Navbar from '$lib/utils/home/navbar.svelte';
+</script>
+
+<Navbar />
+<div class="bg bg-base-300">
+	<div class="min-h-screen">
+		<slot />
+	</div>
+	<Footer />
+</div>
+
+<style>
+	:global(.bg) {
+		background-size: 5px 5px;
+		background-image: radial-gradient(hsla(var(--bc) / 0.1) 0.5px, hsla(var(--b2) / 1) 0.5px);
+	}
+</style>
